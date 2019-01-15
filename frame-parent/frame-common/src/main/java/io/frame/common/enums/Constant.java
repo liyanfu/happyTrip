d66c5148967cc633a3e1135f4c8bf6efdd1ef374
liyf <250977428@qq.com>
2019-01-15 20:39:54 +0800

    tj

--- frame-parent/frame-common/src/main/java/io/frame/common/enums/Constant.java
+++ frame-parent/frame-common/src/main/java/io/frame/common/enums/Constant.java
@@ -13,12 +13,12 @@ public class Constant {
 	 * 云配置类型
 	 */
 	public enum CloudEnum {
-		/** 七牛云 */
-		QINIU(1),
-		/** 阿里云 */
-		ALIYUN(2),
-		/** 腾讯云 */
-		QCLOUD(3);
+	/** 七牛云 */
+	QINIU(1),
+	/** 阿里云 */
+	ALIYUN(2),
+	/** 腾讯云 */
+	QCLOUD(3);
 		private int value;
 
 		CloudEnum(int value) {
@@ -472,4 +472,27 @@ public class Constant {
 		}
 	}
 
+	/**
+	 * 定时任务状态
+	 */
+	public enum ScheduleStatus {
+		/**
+		 * 正常
+		 */
+		NORMAL(0),
+		/**
+		 * 暂停
+		 */
+		PAUSE(1);
+
+		private int value;
+
+		ScheduleStatus(int value) {
+			this.value = value;
+		}
+
+		public int getValue() {
+			return value;
+		}
+	}
 }
